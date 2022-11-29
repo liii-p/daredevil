2b4a5e225ce058b3f3a0dd01af4aedce44c62bf2
lii-p <liannapyman@gmail.com>
2022-11-30 10:14:00 +1030

    Adds basic loading animation upon launch

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import About from "./About/About";
 import History from "./History/History";
 import Associates from "./Associates/Associates";
 import "./Assets/partials/_fonts.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/associates" element={<Associates />} />
-        </Routes>
-      </BrowserRouter>
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/daredevil" element={<About />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/associates" element={<Associates />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
